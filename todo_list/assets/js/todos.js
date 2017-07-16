dc15a4f0fa10bb604a956e4386e503d4c84f8448
Lonnie Currie <lonnie.d.currie@gmail.com>
2017-07-15 21:22:54 -0500

    Update todos.js

--- todo_list/assets/js/todos.js
+++ todo_list/assets/js/todos.js
@@ -1,9 +1,7 @@
-// Check off specific todos by clicking on them
 $("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
-// click on X to delete todo
 $("ul").on("click", "span", function(event){
   event.stopPropagation();
   $(this).parent().fadeOut(500, function(){
@@ -13,11 +11,9 @@ $("ul").on("click", "span", function(event){
 
 $("input[type='text']").keypress(function(event){
   if(event.which === 13){
-    // grabbing new todo list from input
     var todoText = $(this).val();
     var del = "<span><i class='fa fa-trash'></i></span>";
     $(this).val("");
-    //create a new li and add to ul
     $("ul").append("<li>" + del + " " + todoText + "</li>");
   }
 });
